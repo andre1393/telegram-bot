6f8eedaac3e1423e86e7c575105eca2f56f79a59
linux <andre_1393@hotmail.com>
2019-08-09 18:24:34 -0700

    bot can read a image, download and reply to user

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import requests
 import re
 
+def error(update, context):
+    """Log Errors caused by Updates."""
+    print('Update "%s" caused error "%s"', update, context.error)
+
 def submit(bot, update):
-	print("message received")
+	print("submit command received")
 	chat_id = update.message.chat_id
 	bot_token = get_token()
-	message = "mensagem recebida"
-	send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + str(chat_id) + '&parse_mode=Markdown&text=' + message
-	response = requests.get(send_text)
+	message = "comando de submit recebido"
+	update.message.reply_text(message)
 
-	return response
+def process_images(bot, update):
+	print("receipt received")
+	print(type(update.message))
+	photo = None
+	for p in update.message.photo:
+		photo = p
+	
+	# todo: save into a storage
+	photo.get_file().download(custom_path = './receipts/' + photo.file_id)
+
+	update.message.reply_text('Comprovante recebido com sucesso, obrigado ' + update.message.chat.first_name)
 
 def get_token():
 	return open('../token.txt').read()
 
 def main():
-	print('iniciando bot')
-	updater = Updater('930261692:AAFeGW_nb7zsbe_7iExRcrXymRHpozN-F_8')
+	print('starting bot')
+	updater = Updater(get_token())
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler('submit',submit))
+	dp.add_handler(MessageHandler(Filters.photo, process_images))
+	print('bot started')
 	updater.start_polling()
 	updater.idle()
-    
+
 if __name__ == '__main__':
  	main()
